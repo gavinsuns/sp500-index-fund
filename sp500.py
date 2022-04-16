0cc4d9c7d27d94ed526840460ee8afbfae5264f8
gavinsuns <68662120+gavinsuns@users.noreply.github.com>
2022-04-15 22:44:36 -0400

    add imports

---
+++ sp500.py
@@ -0,0 +1,5 @@
+import math #The Python math module
+import numpy as np #The Numpy numerical computing library
+import pandas as pd #The Pandas data science library
+import requests #The requests library for HTTP requests in Python
+import xlsxwriter #The XlsxWriter libarary for
