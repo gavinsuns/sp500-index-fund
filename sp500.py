a825ca8b5b630568f34219b0130aa15c15d4e70e
gavinsuns <gavinsuns@gmail.com>
2022-04-17 17:48:11 -0400

    create excel sheet with the portfolio data

--- sp500.py
+++ sp500.py
@@ -2,13 +2,12 @@ import math #The Python math module
 import numpy as np #The Numpy numerical computing library
 import pandas as pd #The Pandas data science library
 import requests #The requests library for HTTP requests in Python
-import xlsxwriter #The XlsxWriter libarary for
 from secrets import IEX_CLOUD_API_TOKEN
 from utils import chunks
 
 stocks = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]['Symbol'].values.tolist()
 my_columns = ['Ticker', 'Price','Market Capitalization', 'Number Of Shares to Buy']
-final_dataframe = pd.DataFrame(columns = my_columns)
+portfolio_dataframe = pd.DataFrame(columns = my_columns)
 symbol_groups = list(chunks(stocks, 100))
 symbol_strings = []
 
@@ -19,7 +18,7 @@ for symbol_string in symbol_strings:
     batch_api_call_url = f'https://sandbox.iexapis.com/stable/stock/market/batch/?types=quote&symbols={symbol_string}&token={IEX_CLOUD_API_TOKEN}'
     data = requests.get(batch_api_call_url).json()
     for symbol in symbol_string.split(','):
-        final_dataframe = pd.concat([final_dataframe,
+        portfolio_dataframe = pd.concat([portfolio_dataframe,
                                         pd.DataFrame([[symbol, 
                                                    data[symbol]['quote']['latestPrice'], 
                                                    data[symbol]['quote']['marketCap'], 
@@ -35,7 +34,24 @@ while True:
     except ValueError:
         print('That\'s not a number! \n Try again:')
 
-total_rows = len(final_dataframe)
+total_rows = len(portfolio_dataframe)
+
 position_size = float(portfolio_size) / total_rows
+
 for i in range(0, total_rows):
-    final_dataframe['Number Of Shares to Buy'][i] = math.floor(position_size / final_dataframe['Price'][i])
+    portfolio_dataframe['Number Of Shares to Buy'][i] = math.floor(position_size / portfolio_dataframe['Price'][i])
+
+# create excel sheet
+writer = pd.ExcelWriter('sp500.xlsx', engine='xlsxwriter')
+
+# add the portfolio data into the excel sheet
+portfolio_dataframe.to_excel(writer, sheet_name='SP500', index = False, na_rep='NaN')
+
+# auto-adjust column widths
+for column in portfolio_dataframe:
+    column_width = max(portfolio_dataframe[column].astype(str).map(len).max(), len(column))
+    col_idx = portfolio_dataframe.columns.get_loc(column)
+    writer.sheets['my_analysis'].set_column(col_idx, col_idx, column_width)
+
+# save the excel file
+writer.save()
