02d248f9540704b5d526a65f889cb09a9bc322d5
gavinsuns <gavinsuns@gmail.com>
2022-04-18 00:06:09 -0400

    remove numpy

--- sp500.py
+++ sp500.py
@@ -1,5 +1,4 @@
 import math #The Python math module
-import numpy as np #The Numpy numerical computing library
 import pandas as pd #The Pandas data science library
 import requests #The requests library for HTTP requests in Python
 from secrets import IEX_CLOUD_API_TOKEN
